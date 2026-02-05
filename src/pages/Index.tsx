f6a5d01323d0e5a5d20485965201eb68bf241917
thewirrecompany <thewirrecompany@gmail.com>
2026-02-06 00:01:48 +0530

    ready final push 2

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,7 +31,7 @@ export default function Index() {
             WIRRE
           </h1>
           <h2 className="mt-6 text-3xl md:text-5xl font-extrabold tracking-tight text-rose-600 font-mono">
-             Beta Tesitng is Open !
+             Beta Testing is Open!
           </h2>
           <p className="mt-4 text-xl md:text-2xl text-muted-foreground font-mono">
             Compete in Commits.
